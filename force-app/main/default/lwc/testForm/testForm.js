4764a18cc7102341232daee9987001e3dc97c9cc
Raul Juliao <raulj@uninorte.edu.co>
2023-05-29 22:25:26 -0500

    testForm for lightning page

--- force-app/main/default/lwc/testForm/testForm.js
+++ force-app/main/default/lwc/testForm/testForm.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getFieldDisplayValue, getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import NAME_ACCOUNT from '@salesforce/schema/Account.Name'
+import OWNER_NAME from '@salesforce/schema/Account.Owner.Name';
+import ANNUAL_REVENUE_ACCOUNT from '@salesforce/schema/Account.AnnualRevenue';
+export default class TestForm extends LightningElement {
+    name 
+    owner 
+    annual_revenue
+    @api recordId
+    @wire(getRecord, { recordId: '$recordId', fields: [ NAME_ACCOUNT, OWNER_NAME, ANNUAL_REVENUE_ACCOUNT ] } )
+    accountHandler ({error, data}) {
+        if (error) {
+            console.log(error)
+        } else if (data) {
+            console.log(data)
+            this.name = getFieldValue(data, NAME_ACCOUNT);
+            this.owner = getFieldValue(data, OWNER_NAME);
+            this.annual_revenue = getFieldDisplayValue(data.ANNUAL_REVENUE_ACCOUNT);
+        }
+    }
 
-export default class TestForm extends LightningElement {}
+
+}
