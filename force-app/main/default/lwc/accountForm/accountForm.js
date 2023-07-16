261c566c4109c097b105caa29612ec9d08579200
Raul Juliao <raulj@uninorte.edu.co>
2023-07-16 12:48:12 -0500

    JS file with account attributes

--- force-app/main/default/lwc/accountForm/accountForm.js
+++ force-app/main/default/lwc/accountForm/accountForm.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import AC_NAME from '@salesforce/schema/Account.Name';
+import AC_ANNUALREVENUE from '@salesforce/schema/Account.AnnualRevenue';
+import AC_PHONE from '@salesforce/schema/Account.Phone';
+import AC_ACCOUNTSOURCE from '@salesforce/schema/Account.AccountSource'
+import AC_RATING from '@salesforce/schema/Account.Rating'
 
-export default class AccountForm extends LightningElement {}
+export default class AccountForm extends LightningElement {
+    objectName = ACCOUNT_OBJECT;
+    fields = {
+        accountName : AC_NAME,
+        accountAnnualR : AC_ANNUALREVENUE,
+        accountPhone : AC_PHONE,
+        accountAccountSource : AC_ACCOUNTSOURCE,
+        accountRating : AC_RATING
+    }
+}
